411b904b7217b12eb2e0f7463387f34dc8d958ca
ZPAC <77367524+ZPACGaming@users.noreply.github.com>
2021-01-12 20:32:42 -0500

    Update WaterBlock.js

--- WaterBlock.js
+++ WaterBlock.js
@@ -1,1 +1,3 @@
-
+Block.defineBlock(404, "Water", [["still_water", 0], ["still_water", 0], ["still_water", 0], ["still_water", 0], ["still_water", 0], ["still_water", 0]]);
+Block.setLightLevel(404, 0);
+Block.setDestroyTime(404, 100);
