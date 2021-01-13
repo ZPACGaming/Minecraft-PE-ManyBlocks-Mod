e849e7a8dcac4b928f483e93c6198c3323e12c5e
ZPAC <77367524+ZPACGaming@users.noreply.github.com>
2021-01-12 20:10:25 -0500

    Update GlowingObsidian.js

--- GlowingObsidian.js
+++ GlowingObsidian.js
@@ -1,1 +1,3 @@
-
+Block.defineBlock(401, "Glowing Obsidian", [["glowing_obsidian", 0], ["glowing_obsidian", 0], ["glowing_obsidian", 0], ["glowing_obsidian", 0], ["glowing_obsidian", 0], ["glowing_obsidian", 0]]);
+Block.setLightLevel(401, 15);
+Block.setDestroyTime(401, 90);
