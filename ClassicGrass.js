0226cadb6e497c90963fd64388666c9f129d82b8
ZPAC <77367524+ZPACGaming@users.noreply.github.com>
2021-01-12 20:26:12 -0500

    Update ClassicGrass.js

--- ClassicGrass.js
+++ ClassicGrass.js
@@ -1,1 +1,3 @@
-
+Block.defineBlock(403, "Classic Grass", [["grass", 2], ["grass", 2], ["grass", 2], ["grass", 2], ["grass", 2], ["grass", 2]]);
+Block.setLightLevel(403, 0);
+Block.setDestroyTime(403, 5);
