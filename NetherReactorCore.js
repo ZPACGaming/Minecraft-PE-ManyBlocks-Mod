2b6e1ed3e4c6a4e1876f1e73b1b829c8b73a3f72
ZPAC <77367524+ZPACGaming@users.noreply.github.com>
2021-01-12 20:21:47 -0500

    Update NetherReactorCore.js

--- NetherReactorCore.js
+++ NetherReactorCore.js
@@ -1,1 +1,3 @@
-
+Block.defineBlock(402, "Nether Reactor Core", [["reactor_core", 2], ["reactor_core", 2], ["reactor_core", 0], ["reactor_core", 0], ["reactor_core", 0], ["reactor_core", 0]]);
+Block.setLightLevel(402, 0);
+Block.setDestroyTime(402, 10);
