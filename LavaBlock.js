6830bf0c95208377634d2c4348026d5d171774a5
ZPAC <77367524+ZPACGaming@users.noreply.github.com>
2021-01-13 11:12:01 -0500

    Update LavaBlock.js

--- LavaBlock.js
+++ LavaBlock.js
@@ -1,1 +1,3 @@
-
+Block.defineBlock(405, "Lava", [["still_lava", 0], ["still_lava", 0], ["still_lava", 0], ["still_lava", 0], ["still_lava", 0], ["still_lava", 0]]);
+Block.setLightLevel(405, 15);
+Block.setDestroyTime(405, 100);
